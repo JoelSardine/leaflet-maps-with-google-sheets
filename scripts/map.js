1e448c2fa5b1ac64e87a196d9e826cfe465994a6
JoelSardine <111348310+JoelSardine@users.noreply.github.com>
2022-08-25 15:40:07 +0100

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -900,8 +900,22 @@ $(window).on('load', function() {
    /**
    * Changes map attribution (author, GitHub repo, email etc.) in bottom-right
    */
-  new L.Map('container', {
-    'attributionControl': false
+    function changeAttribution() {
+    var attributionHTML = $('.leaflet-control-attribution').hide()
+
+    if (name && url) {
+      if (url.indexOf('@') > 0) { url = 'mailto:' + url; }
+      credit += ' by <a href="' + url + '">' + name + '</a> | ';
+    } else if (name) {
+      credit += ' by ' + name + ' | ';
+    } else {
+      credit += ' | ';
+    }
+
+    credit += 'View <a href="' + getSetting('_githubRepo') + '">code</a>';
+    if (getSetting('_codeCredit')) credit += ' by ' + getSetting('_codeCredit');
+    credit += ' with ';
+    $('.leaflet-control-attribution')[0].innerHTML = credit + attributionHTML;
   }
 
 /**
